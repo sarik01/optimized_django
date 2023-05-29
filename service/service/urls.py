fd7e241d83b3943cf34c684a93b2d0bb06c4d8a7
sarva <sarvar_kamilov2@mail.ru>
2023-05-29 10:44:46 -0800

    23.05.23 add celery

--- service/service/urls.py
+++ service/service/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from services.views import SubscriptionView
+
+
+router = routers.DefaultRouter()
+router.register(r'api/subscriptions', SubscriptionView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
+
+
+
